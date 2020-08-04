a0ca75096fbeef5bf7a996ba40298861d2322a7c
jhkim <jhkim@192.168.56.1>
2020-08-04 16:25:36 +0900

    asdf

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -10,5 +10,10 @@ public class HelloController {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
 
 }
